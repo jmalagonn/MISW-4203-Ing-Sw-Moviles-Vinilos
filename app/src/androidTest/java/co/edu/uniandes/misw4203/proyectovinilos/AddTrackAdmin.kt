01622eb446ffef0d7451eaa0d4478d0b053a45d0
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2024-11-03 21:57:50 -0500

    Corrección de Test Espresso HU01

--- app/src/androidTest/java/co/edu/uniandes/misw4203/proyectovinilos/AddTrackAdmin.kt
+++ app/src/androidTest/java/co/edu/uniandes/misw4203/proyectovinilos/AddTrackAdmin.kt
@@ -3,10 +3,12 @@ package co.edu.uniandes.misw4203.proyectovinilos
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.replaceText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -47,7 +49,7 @@ class AddTrackAdmin {
             .perform(click())
 
         // Selección aleatoria del índice del álbum entre 1 y 15
-        val randomAlbumPosition = Random.nextInt(1, 15)
+        val randomAlbumPosition = Random.nextInt(1, 9)
 
         val recyclerView = onView(
             allOf(
@@ -55,10 +57,9 @@ class AddTrackAdmin {
                 isDisplayed()
             )
         )
+
         Thread.sleep(3000)
-
         recyclerView.perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(randomAlbumPosition, click()))
-
         Thread.sleep(4000)
 
         // Verificar la existencia del boton agregar track
--- app/src/androidTest/java/co/edu/uniandes/misw4203/proyectovinilos/ViewDetailAlbumVisitor.kt
+++ app/src/androidTest/java/co/edu/uniandes/misw4203/proyectovinilos/ViewDetailAlbumVisitor.kt
@@ -1,12 +1,15 @@
 package co.edu.uniandes.misw4203.proyectovinilos
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
@@ -43,7 +46,7 @@ class ViewDetailAlbumVisitor {
         materialTextView.perform(click())
 
         // Selección aleatoria del índice del álbum entre 1 y 15
-        val randomAlbumPosition = Random.nextInt(1, 15)
+        val randomAlbumPosition = Random.nextInt(1, 9)
 
         val recyclerView = onView(
             allOf(
@@ -52,7 +55,7 @@ class ViewDetailAlbumVisitor {
             )
         )
         Thread.sleep(2000)
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(randomAlbumPosition, click()))
+        recyclerView.perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(randomAlbumPosition, click()))
         Thread.sleep(2000)
 
         // Verificar la existencia de los elementos, album, fecha de lanzamiento, disquera, albumcover y descripcion
