6bc48fec30acf2d56f986e9f47e019b0f29f97b9
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2024-11-13 19:22:09 -0500

    Pruebas espresso con conexión a internet

---
+++ app/src/androidTest/java/co/edu/uniandes/misw4203/proyectovinilos/AllTestsSuiteHU03HU04.kt
@@ -0,0 +1,16 @@
+package co.edu.uniandes.misw4203.proyectovinilos
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    LoginViewArtist::class,
+    LoginFilterArtist::class,
+    LoginViewArtistOffline::class,
+    ViewDetailArtist::class,
+
+
+    )
+
+class AllTestsSuiteHU03HU04
